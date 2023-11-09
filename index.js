110273188b3bb147011fe4705b0bcb8ebf8c8dc2
Abdul Rahman <abdulrahmans0414@gmail.com>
2023-11-10 01:52:12 +0530

    add buttonAnimataion and setTimeout

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ for (var i = 0; i < numberOfDrumButtons; i++) {
 
         makeSound(buttonInnerHTML);
 
+        buttonAnimation(buttonInnerHTML);
+
     });
 
 }
@@ -17,6 +19,7 @@ for (var i = 0; i < numberOfDrumButtons; i++) {
 
 document.addEventListener("keypress", function (event) {
     makeSound(event.key);
+    buttonAnimation(event.key);
 });
 
 
@@ -59,7 +62,19 @@ function makeSound(key) {
             break;
 
 
-        default: console.log(buttonInnerHTML);
+        default: console.log(key);
 
     }
+}
+
+function buttonAnimation(currentKey) {
+
+    var activeButton = document.querySelector("." + currentKey);
+
+    activeButton.classList.add("pressed");
+
+    setTimeout(function () {
+        activeButton.classList.remove("pressed");
+    }, 100);
+
 }
