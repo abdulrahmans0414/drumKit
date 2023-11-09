ccf6c80dbda5012ce56921fd932a8c7f61d43fb0
Abdul Rahman <abdulrahmans0414@gmail.com>
2023-11-09 16:52:42 +0530

    add audio

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ var numberOfDrumButtons = document.querySelectorAll(".drum").length;
 for (var i = 0; i < numberOfDrumButtons; i++) {
 
     document.querySelectorAll(".drum")[i].addEventListener("click", () => {
-        alert("i got clicked!");
+        
+       var audio = new Audio("sounds/tom-1.mp3");
+       audio.play();
 
     });
 
